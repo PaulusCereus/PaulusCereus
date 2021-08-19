d624f43c5c3f7d95e668a5e58f1626ea18c0ee16
PaulusCereus <89133329+PaulusCereus@users.noreply.github.com>
2021-08-19 19:40:01 +0300

    Update python_model.py

--- python_model.py
+++ python_model.py
@@ -15,7 +15,7 @@ import re
 
 df = pd.read_csv('/content/drive/MyDrive/basketball.csv', encoding= 'cp1251', sep=';', header=0, index_col=0)
 
-Xtrain = np.array(df[['Ком. 1','Ком. 2', 'ftime']].astype('int'))
+Xtrain = np.array(df[['COM. 1','COM. 2', 'ftime']].astype('int'))
 Ytrain = np.array(df['fcount'].astype('int'))
 
 x_train, x_test, y_train, y_test = train_test_split(Xtrain, Ytrain, test_size=0.05)
