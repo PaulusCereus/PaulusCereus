61a4af588384f52972a96b618624ecc6f1ce0b66
PaulusCereus <89133329+PaulusCereus@users.noreply.github.com>
2021-08-19 21:10:52 +0300

    Add files via upload

--- cpp_model.cc
+++ cpp_model.cc
@@ -1,15 +1,18 @@
 #include "src/model.h"
-
+#include <iostream>
 using keras2cpp::Model;
 using keras2cpp::Tensor;
 
 int main() {
     // Initialize model.
     auto model = Model::load("example.model");
-
+    float a, b, c;
     // Create a 1D Tensor on length 10 for input data.
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
     Tensor in{3};
-    in.data_ = {0, 2.0, 90.0};
+    in.data_ = {a, b, c};
 
     // Run prediction.
     Tensor out = model(in);
