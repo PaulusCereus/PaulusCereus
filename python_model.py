410de2a9af10555e5a78a221bc4f3091622f9bf8
PaulusCereus <89133329+PaulusCereus@users.noreply.github.com>
2021-08-19 20:46:47 +0300

    Add files via upload

--- python_model.py
+++ python_model.py
@@ -13,8 +13,8 @@ import math
 import os
 import re
 
-df = pd.read_csv('/content/drive/MyDrive/basket.csv', encoding= 'cp1251', sep=';', header=0, index_col=0)
-
+df = pd.read_csv('/content/drive/MyDrive/basket.csv', encoding= 'cp1251', sep=',', header=0, index_col=0)
+print(df)
 Xtrain = np.array(df[['COM 1','COM 2', 'ftime']].astype('int'))
 Ytrain = np.array(df['fcount'].astype('int'))
 
